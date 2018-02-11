422a1910138a8da133296db0340f1074f4db7474
Tudor <tudor.suditu@yahoo.com>
2018-02-11 13:28:37 +0200

    added auth configurations for user management

--- config/defaults.js
+++ config/defaults.js
@@ -1,7 +1,11 @@
 const environment = process.env.NODE_ENV || 'localhost';
 module.exports = {
-    port: process.env.PORT || 3005,
+    port: process.env.PORT || 3006,
     tracingHeaderKey: process.env.TRACING_HEADER_KEY || 'x-ss-request-id',
     jwtHeaderKey: process.env.JWT_HEADER_KEY || 'x-ss-jwt',
-    SERVICE_NAME: 'stay-safe-auth'
+    serviceName: 'stay-safe-auth',
+    managementClientId: process.env.MANAGEMENT_CLIENT_ID,
+    managementClientSecret: process.env.MANAGEMENT_CLIENT_SECRET,
+    managementRealm: process.env.MANAGEMENT_REALM,
+    authServerUrl: process.env.AUTH_SERVER_URL || 'http://localhost:8080/auth'
 };
